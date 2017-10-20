5b37989c2657d3db8ee3f390cb2da895eaa5eaef
Matt Faircloth <mattcfaircloth@gmail.com>
2017-10-20 18:03:20 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(number)
-  counter = number
+  #counter = number
   while counter >= 0
     puts "#{number} SECOND(S)!"
-    counter -= 1
+    number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
